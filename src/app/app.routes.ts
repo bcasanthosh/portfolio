40b0fad21aa4c85422607f9ff04948901d30918c
unknown <santhoshbca9384@gmail.com>
2023-12-11 17:46:00 +0530

    my portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Header',component:HeaderComponent},
+    {path:'frontpage',component:FrontpageComponent},
+    {path:'footer',component:FooterComponent}
+];

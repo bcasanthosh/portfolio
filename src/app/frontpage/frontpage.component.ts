9ec22c8976ac3b0d36204c5e8375bd9dbd31289b
unknown <santhoshbca9384@gmail.com>
2023-12-11 16:52:25 +0530

    portfolio

---
+++ src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-frontpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './frontpage.component.html',
+  styleUrl: './frontpage.component.scss'
+})
+export class FrontpageComponent {
+
+}
